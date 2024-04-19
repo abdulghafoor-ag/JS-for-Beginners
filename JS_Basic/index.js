e3d013894a746dda96e4b410cf0bd09f13aec9b7
Abdul Ghafoor <149339826+abdulghafoor-ag@users.noreply.github.com>
2024-04-19 17:25:16 +0500

    Add files via upload

---
+++ JS_Basic/index.js
@@ -0,0 +1,85 @@
+ //This is my first code! ----------------------------------------
+//  console.log('Hello World');
+
+//  let name = 'Mr:';
+ 
+
+//  let firstName = "Ghafoor";
+//  let lastName = "Soomro";
+
+// console.log(name,firstName,lastName);
+
+// let age = 12;
+// age = 20;
+// console.log(age);
+
+
+// let can be change but const cannot be change following code will give type Error -----------
+// const myage = 12;
+// myage = 20;
+// console.log(myage);
+
+//Premitive Types/ Value Types (String, Number, Boolean, Undefined, Null) ---------------------------------------------
+
+
+// let name = 'Ghafoor';   // string littral
+// let age = 22;           // number littral
+// let isAccepted = false; // boolean littral
+// let designation = undefined; // undefined littral
+// let selectedPost = null;    // null littral
+
+// Refrance Types (Objects, array, Functions) ---------
+
+//Object------ 
+// let person =
+// {   name: 'Ghafoor',
+//     age : 12
+// };
+// console.log(person);
+
+// //Dot Notation
+// person.age = 15
+// console.log(person.age);
+
+// // Braket Notation 
+// person['name'] = 'Soomro'
+// console.log(person.name)
+
+// console.log(person);
+
+
+
+// Array ----
+
+// let favoriteFood = ['Rice', 'Vagitables'];
+// console.log(favoriteFood);
+// console.log(favoriteFood[0]);
+// favoriteFood[2] = 'Pizza' // add an element in array
+// console.log(favoriteFood);
+
+// console.log(favoriteFood.length);
+
+//Function --------
+// performing a Task 
+
+// function greet(){
+//     console.log('Hello World')
+// }
+// greet();
+
+// function greetings(name, cast){
+
+//     console.log('Hello ' + name + ' '+ cast)
+// }
+// greetings('Ghafoor','Soomro');
+
+// calculation 
+function square(number){
+    return number*number
+}
+
+let number = square(10);
+console.log(number);
+
+console.log(square(2));
+
